89f3853d15acde75cca936f2a9f17e13ec92caf4
runaarai <luna.alay1006@gmail.com>
2025-08-25 14:40:42 +0900

    おみくじ結果カウント

--- omikuji/Program.cs
+++ omikuji/Program.cs
@@ -12,8 +12,28 @@ omikuji[4] = "4大凶";
 int[] count = new int[5];
 //Randomクラスを実体化して変数randが機能を参照できる
 Random rand = new Random();
-int result = rand.Next(0, 5);//０以上５未満のランダムを取得して変数resultに代入
 
-//result番目の値を配列から引っ張ってきてコンソールに出す
-Console.WriteLine("今日の運勢は… " + omikuji[result]);
+for (int i = 0; i < 100; i++)
+{
+    int result = rand.Next(0, 5);//０以上５未満のランダムを取得して変数resultに代入
 
+    //result番目の値を配列から引っ張ってきてコンソールに出す
+    Console.WriteLine(omikuji[result]);
+    //resultで引いた結果の番号を何回引いたかカウント
+    count[result]++;
+}
+
+
+// Console.WriteLine("今日の運勢は… " + omikuji[result]);
+
+// for (int i = 0; i < 100; i++)
+// {
+//     Console.WriteLine("今" + i + "回目の繰り返しです");
+// }
+
+Console.WriteLine("\n=== 集計結果 ===");
+// 配列countの結果を順番に発表
+for (int i = 0; i < omikuji.Length; i++)
+{
+    Console.WriteLine($"{omikuji[i]}: {count[i]}回");
+}
--- omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
+++ omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac38dbfb613aa6439e0f9b6e15f94200250d142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ead4f79f58f13a52d06fa9f547a6333f8b1f75")]
 [assembly: System.Reflection.AssemblyProductAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
